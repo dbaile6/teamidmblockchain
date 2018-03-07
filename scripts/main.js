27a74898be5b1327888f5ffa2dc4b55a5ad75484
Dylan Bailey <dylanba1@gmail.com>
2018-03-07 14:36:30 -0500

    Added clock and date.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
 
-})
+function displayClock () {var d = new Date();
+ap = "am";
+h = d.getHours();
+m = d.getMinutes();
+s = d.getSeconds();
+da = d.getDate();
+mo = d.getMonth();
+y = d.getFullYear();
+if (h > 11) { ap = "pm"; }
+if (h > 12) { h = h - 12; }
+if (h == 0) { h = 12; }
+if (m < 10) { m = "0" + m; }
+if (s < 10) { s = "0" + s; }
+var myTime = document.getElementById('clock').innerHTML = + mo + "/" + da + "/" + y + "                           " + h + ":" + m + ":" + s + " " + ap;
+}
+
+setInterval(displayClock, 1000);
